a6554061976354c80e0077b2ad369aaf6e472535
Daniel <danieleduardocald@gmail.com>
2021-02-02 09:59:47 -0500

    commit tree d9ac6e2d22695fbb39e06358bc0726271275c44e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+let dodger = document.getElementbyId ('dodger');
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt (leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `S{left-1}px`;
+    };
+};
+
